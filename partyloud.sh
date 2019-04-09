4f2b378c74a0fcef65a09961126571a0a64f867e
THO <dave.tho@protonmail.com>
2019-04-09 15:45:33 +0200

    minor fixes, less buggy

--- partyloud.sh
+++ partyloud.sh
@@ -5,7 +5,7 @@ readonly BW="$(cat badwords)"
 readonly BW_S="$(wc -l < badwords)"
 
 # UI FUNCTION
-function logo() {
+logo() {
 cat << 'EOF'
 
   ___          _        _                _
@@ -23,7 +23,7 @@ simulate navigation
 EOF
 }
 
-function DisplayHelp() {
+DisplayHelp() {
     cat << 'EOF'
 ==================[ HELP ]==================
 
@@ -35,13 +35,13 @@ function DisplayHelp() {
 EOF
 }
 
-function center() {
+center() {
     columns="$(tput cols)"
     printf "%*s" "$(( (${#1} - columns) / 2))"
     echo -ne "$1"
 }
 
-function clearLines() {
+clearLines() {
     for ((x=1; x<="$1"; x++)); do
         if [ "$x" != "$1" ]
         then
@@ -56,27 +56,27 @@ function progress() {
     echo -ne "$1 [$2]"
 }
 
-function generateUserAgent() {
-    readonly WIN=( "10.0"  # Win10
+generateUserAgent() {
+    local -r WIN=( "10.0"  # Win10
                    "6.3"   # Win 8.1
                    "6.2"   # Win 8
                    "6.1" ) # Win 7
 
-    readonly MAC=( "10_14" "10_14_1" "10_14_2" "10_14_3" "10_14_4" "10_14_5" "10_14_6"  # Mojave
+    local -r MAC=( "10_14" "10_14_1" "10_14_2" "10_14_3" "10_14_4" "10_14_5" "10_14_6"  # Mojave
                    "10_13" "10_13_1" "10_13_2" "10_13_3" "10_13_4" "10_13_5" "10_13_6"  # High Sierra
                    "10_12" "10_12_1" "10_12_2" "10_12_3" "10_12_4" "10_12_5" "10_12_6"  # Sierra
                    "10_11" "10_11_1" "10_11_2" "10_11_3" "10_11_4" "10_11_5" "10_11_6") # El Capitan
 
     local UserAgent="Mozilla/5.0 "
 
-    local OS=$(( RANDOM % 3 + 1))
-    readonly bit=$(( RANDOM % 2))
+    local OS="$(( RANDOM % 3 + 1 ))"
+    local -r bit="$(( RANDOM % 2 ))"
 
-    if [ "$OS" == 1 ]
+    if [ "${OS}" == 1 ]
     then
         # Windows
         OS="${WIN[$(( RANDOM % ${#WIN[@]} ))]}"
-        if [ "$bit" == 0 ]
+        if [ "${bit}" == 0 ]
         then
             # 32bit
             UserAgent+="(Windows NT $OS"
@@ -84,14 +84,14 @@ function generateUserAgent() {
             # 64bit
             UserAgent+="(Windows NT $OS; Win64; x64"
         fi
-    elif [ "$OS" == 2 ]
+    elif [ "${OS}" == 2 ]
     then
         # MacOS
         OS="${MAC[$(( RANDOM % ${#MAC[@]} ))]}"
         UserAgent+="(Macintosh; Intel Mac OS X $OS"
     else
         # Linux
-        if [ "$bit" == 0 ]
+        if [ "${bit}" == 0 ]
         then
             # 32bit
             UserAgent+="(X11; Linux i686"
@@ -101,10 +101,10 @@ function generateUserAgent() {
         fi
     fi
 
-    readonly Browser=$(( RANDOM % 2 ))
+    local -r Browser="$(( RANDOM % 2 ))"
     local VER=""
 
-    if [ "$Browser" == 1 ]
+    if [ "${Browser}" == 1 ]
     then
         # Firefox
         readonly FF=( "50.0" "50.0.1" "50.0.2" "50.1.0"
@@ -125,7 +125,7 @@ function generateUserAgent() {
                       "65.0" "65.0.1" "65.0.2"
                       "66.0" )
         VER="${FF[$(( RANDOM % ${#FF[@]} ))]}"
-        UserAgent+="; rv:$VER) Gecko/20100101 Firefox/$VER"
+        UserAgent+="; rv:${VER}) Gecko/20100101 Firefox/${VER}"
     else
         # Chrome
         readonly CH=( "56.0.2924" "57.0.2987" "58.0.3029"
@@ -135,74 +135,79 @@ function generateUserAgent() {
                       "68.0.3440" "69.0.3497" "70.0.3538"
                       "71.0.3578" "72.0.3626" "73.0.3683" )
         VER="${CH[$(( RANDOM % ${#CH[@]} ))]}"
-        UserAgent+=") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/$VER Safari/537.36"
+        UserAgent+=") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${VER} Safari/537.36"
     fi
 
-    echo "$UserAgent"
+    echo "${UserAgent}"
 }
 
-function getLock() {
+getLock() {
     while ! mkdir /tmp/partyloud.lock 2>/dev/null
     do
         sleep 0.2
     done
 }
 
-function freeLock() {
+freeLock() {
     rm -fr /tmp/partyloud.lock
 }
 
 
-function stop() {
+stop() {
     for PID in ${PIDS[@]};
     do
-        progress "[+] Terminating HTTP Engines ..." "pid: $PID"
+        progress "[+] Terminating HTTP Engines ..." "pid: ${PID}"
         kill -9 "$PID"
         wait "$PID" 2>/dev/null
     done
 }
 
-function filter() {
-    local URLS="$1"
-    for FILTER in ${BW[@]};
-    do
-        URLS="$(grep -v "$FILTER" <<< "$URLS")"
-    done
-    echo "$URLS"
+filter() {
+    local URLS="$(grep -iv "${2}" <<< "${1}")"
+    if [[ "${URLS}" != "" ]]
+    then
+        for FILTER in ${BW[@]};
+        do
+            URLS="$(grep -iv "${FILTER}" <<< "${URLS}")"
+        done
+    fi
+    echo "${URLS}"
 }
 
-function Engine() {
-    local URL="$1"
-    local ALT="$1"
+Engine() {
+    local URL="${1}"
+    local ALT="${3}"
     local RES=""
     local NUM=0
     local WORDS=0
     local SIZE=0
+    local -r URL_REGEX='\b(https|http)://[A-Za-z0-9_|.]*(/([^\.\"?:;,#\<\>=% ]*(.html)?)*)'
     while true; do
         getLock
-        RES="$(curl -L -A "$2" -w '%{http_code}' "$URL" 2>&1)"
+        RES="$(curl -L -A "${2}" -w '%{http_code}' "${URL}" 2>&1)" || "$(echo "")"
         freeLock
-        echo -ne "$URL -> "
-        if [[ "${RES:(-3)}" == "200" ]]
+        echo "${URL} : ${RES:(-3)}"
+        if [[ "${RES}" != "" ]] && [[ "${RES:(-3)}" == "200" ]]
         then
-            RES="$(grep -Eo '\b(https|http)://[-A-Za-z0-9_|.]*[-A-Za-z0-9+_|](/([^\."?:;,\<\>]*(.html)?)?)' <<< "$RES" | sort | uniq)"
-            RES="$(filter "$RES")"
-            SIZE="$(wc -l <<< "$RES")"
+            RES="$(awk -F '"' '{print $2}' <<< ${RES})"
+            RES="$(grep -Eo "${URL_REGEX}" <<< "${RES}" | sort | uniq)"
+            RES="$(filter "${RES}" "${URL}")"
+            SIZE="$(wc -l <<< "${RES}")"
             #echo "$RES"
-            if [[ $SIZE -gt 3 ]]
+            if [[ "${SIZE}" -gt 3 ]]
             then
-                ALT="$URL"
-                NUM="$(( RANDOM % $(( $SIZE - 1 )) + 1 ))"
-                URL="$(sed "${NUM}q;d" <<< $RES)" # Random Link
+                ALT="${URL}"
+                NUM="$(( RANDOM % $(( ${SIZE} - 1 )) + 1 ))"
+                URL="$(sed "${NUM}q;d" <<< ${RES})" # Random Link
             else
-                URL="$ALT"
-                ALT="$1"
+                URL="${ALT}"
+                ALT="${3}"
             fi
         else
-            URL="$ALT"
-            ALT="$1"
+            URL="${ALT}"
+            ALT="${3}"
         fi
-        echo "$URL"
+        #echo "${URL}"
         WORDS="$(( RANDOM % 100 + 150 ))" # Guessing words on the web page
         NUM="0.$(( RANDOM % 1000 + 3500 ))" # Guessing read speed
 
@@ -220,13 +225,15 @@ function main() {
     trap stop SIGTERM
     trap stop EXIT
 
-    local -r L_LEN="$(( $(wc -l < partyloud.conf) -1 ))"
+    local -r L_LEN="$(( $(wc -l < partyloud.conf) - 1 ))"
     local CURR_URL=""
+    local ALT_URL=""
 
     for ((i=1; i<=$1; i++)); do
-        CURR_URL="$(sed "$(( RANDOM % L_LEN + 1))q;d" < partyloud.conf)"
-       # progress "[+] Starting HTTP Engine ($CURR_URL) ... " "$i/$1"
-        Engine "$CURR_URL" "$(generateUserAgent)" &
+        CURR_URL="$(sed "$(( RANDOM % ${L_LEN} + 1))q;d" < partyloud.conf)"
+        ALT_URL="$(sed "$(( RANDOM % ${L_LEN} + 1))q;d" < partyloud.conf)"
+        # progress "[+] Starting HTTP Engine ($CURR_URL) ... " "$i/$1"
+        Engine "${CURR_URL}" "$(generateUserAgent)" "${ALT_URL}" &
         PIDS+=("$!")
         sleep 0.2
     done
@@ -235,7 +242,7 @@ function main() {
    # echo -ne "[+] HTTP Engines Started!\n"
 
     local RESPONSE=""
-    echo -ne "\n\n"
+   # echo -ne "\n\n"
 
     #center "[ PRESS ENTER TO STOP ]"
     read -r RESPONSE
@@ -252,15 +259,15 @@ logo
 
 rm -fr /tmp/partyloud.lock
 
-if [ $# == 1 ]
+if [[ "${#}" == 1 ]]
 then
-    if [[ "$1" -gt 0 ]] && [[ "$1" -lt 25 ]]
+    if [[ "${1}" -gt 0 ]] && [[ "${1}" -lt 25 ]]
     then
-        main "$1"
+        main "${1}"
     else
         DisplayHelp
     fi
-elif [ $# == 0 ]
+elif [ "${#}" == 0 ]
 then
     main 7
 else
