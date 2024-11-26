13861b1c147291b1808fab489f0a9b04fb2e053a
Davide Olgiati <davide.olgiati@hotmail.it>
2024-11-26 21:50:08 +0100

    copyright update

--- partyloud.sh
+++ partyloud.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2019 THO
+# Copyright (C) 2024 Davide Olgiati
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
